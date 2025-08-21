64b13da46994ed19e476dbf2cd7417886994528f
nwctrinity <severmay123@mail.ru>
2025-08-22 04:33:30 +0500

    feat: добавил хедер

---
+++ src/widgets/header/types.ts
@@ -0,0 +1,10 @@
+export type HeaderProps = {
+  onLogoClick?: () => void;
+  onAboutClick?: () => void;
+  onAllSkillsClick?: () => void;
+  onSearch: () => void;
+  onThemeToggle?: () => void;
+  onLogin?: () => void;
+  onRegister?: () => void;
+  darkTheme?: boolean;
+};

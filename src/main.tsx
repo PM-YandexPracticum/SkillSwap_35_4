04693c526f2842f429921128e508eca24f0fa4d9
Timur Belov <timur.belov.16@bk.ru>
2025-08-22 02:00:30 +0300

    добавил компонент header

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app/App.tsx';
+//import App from './app/App.tsx';
 import './index.scss';
+import { Header } from './components/header/Header.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Header/>
     </BrowserRouter>
   </StrictMode>,
 );

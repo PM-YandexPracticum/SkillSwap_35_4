bbfb95061fad475926ead350c7b879f203104634
Elena Romanova <romanova.bim@gmail.com>
2025-08-21 14:10:40 +0500

    добавлен попап для уведомлений NotificationPopup

---
+++ src/shared/ui/NotificationPopup/NotificationPopup.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { NotificationPopup } from './NotificationPopup';
+
+const meta: Meta<typeof NotificationPopup> = {
+  title: 'Shared/NotificationPopup',
+  component: NotificationPopup,
+  tags: ['autodocs'],
+  args: {
+    message: 'Олег предлагает вам обмен',
+    onClose: () => alert('Нажали кнопку Крестик'),
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof NotificationPopup>;
+
+export const Default: Story = {
+  name: 'Уведомление без кнопки перехода',
+  args: {
+    message: 'Олег предлагает вам обмен',
+  },
+};
+
+export const LongText: Story = {
+  name: 'Уведомление с длинным текстом',
+  args: {
+    message:
+      'Очень длинное уведомление, чтобы проверить как ведет себя компонент при переполнении текста. ',
+  },
+};
+
+export const WithNavigate: Story = {
+  name: 'Уведомление с кнопкой перехода',
+  args: {
+    message: 'Олег предлагает вам обмен',
+    onNavigate: () => alert('Нажали кнопку Перейти'),
+  },
+};

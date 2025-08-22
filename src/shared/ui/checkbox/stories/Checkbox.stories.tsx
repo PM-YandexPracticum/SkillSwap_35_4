008a95e94c0b2ff2217a148610c25ed6be018bb3
Aleksei Zakharchenko <ckill071@gmail.com>
2025-08-22 03:35:39 +0300

    move storis from src/storis to components folders

--- src/shared/ui/checkbox/stories/Checkbox.stories.tsx
+++ src/shared/ui/checkbox/stories/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Checkbox } from '../shared/ui/checkbox/checkbox';
+import { Checkbox } from '../checkbox';
 import { useState } from 'react';
 
 const meta: Meta<typeof Checkbox> = {

105248075791cd5339dce7589eeae8572468ea2c
Nikita-Tig <nikitatig2005@gmail.com>
2025-08-28 18:46:06 +0300

    fix: поправил и добавил логики для работы с навыком

--- src/services/skillsSlice/skillsSlice.ts
+++ src/services/skillsSlice/skillsSlice.ts
@@ -1,25 +1,46 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import type { Subcategory } from "../../api/types";
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { Subcategory } from '../../api/types';
 
-type SkillsState = {
-  skillCanTeach: Subcategory[];
-};
+interface SkillsState {
+  skillCanTeach: Subcategory;
+  //  описание навыка (если пригодится)
+  description: string;
+  //  уникальный идентификатор для навыка
+  uid: number;
+}
 
-const initialUsersSkillsState: SkillsState = {
-  skillCanTeach: [],
-};
+const initialUsersSkillsState: SkillsState[] = [
+  {
+    skillCanTeach: { id: 0, name: '' },
+    description: '',
+    uid: 0,
+  },
+];
 
 export const skillsSlice = createSlice({
   name: 'skills',
   initialState: initialUsersSkillsState,
   reducers: {
-    addSkill: (state, action: PayloadAction<Subcategory>) => {
-      state.skillCanTeach.push(action.payload);
-      localStorage.setItem('userSkills', JSON.stringify(state.skillCanTeach));
+    addSkill: {
+      reducer: (state, action: PayloadAction<SkillsState>) => {
+        state.push(action.payload);
+        localStorage.setItem('userSkills', JSON.stringify(state));
+      },
+      prepare: (skill: Subcategory & { description: string }) => {
+        // создание уникального идентификатора для навыка
+        const uid = Date.now();
+        return {
+          payload: {
+            skillCanTeach: { id: skill.id, name: skill.name },
+            description: skill.description ? skill.description : '',
+            uid,
+          },
+        };
+      },
     },
     removeSkill: (state, action: PayloadAction<number>) => {
-      state.skillCanTeach = state.skillCanTeach.filter(skill => skill.id !== action.payload);
-      localStorage.setItem('userSkills', JSON.stringify(state.skillCanTeach));
+      state = state.filter((skill) => skill.uid !== action.payload);
+      localStorage.setItem('userSkills', JSON.stringify(state));
     },
   },
 });
